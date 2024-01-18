2efc072e72eec4e0ab02a4743f68e11d3fe31149
doctogon <multilayerdreyer@gmail.com>
2024-01-18 21:54:45 +0800

    test

--- script/location.js
+++ script/location.js
@@ -1,9 +1,9 @@
-// url: https://api.ipgeolocation.io/ipgeo?apiKey=e88203af81524391a7641693aa2d7430&ip=<ADD IP HERE>
+// // url: https://api.ipgeolocation.io/ipgeo?apiKey=e88203af81524391a7641693aa2d7430&ip=<ADD IP HERE>
 
-// find ip of the user
-function getIPFromAmazon() {
-    fetch("https://checkip.amazonaws.com/").then(res => res.text()).then(data => console.log(data))
-  }
+// // find ip of the user
+// function getIPFromAmazon() {
+//     fetch("https://checkip.amazonaws.com/").then(res => res.text()).then(data => console.log(data))
+//   }
 
   
-document.getElementById("#gfg").innerHTML = getIPFromAmazon();
+// document.getElementById("#gfg").innerHTML = getIPFromAmazon();
