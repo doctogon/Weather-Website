aab50755b597eecf7ca84ce00de4f5341645d3c5
Andy <66284061+flipperflopper@users.noreply.github.com>
2024-01-19 21:25:33 +0800

    gett the weather from api

--- script/location.js
+++ script/location.js
@@ -1,7 +1,21 @@
+// get latitude & longitute
 navigator.geolocation.getCurrentPosition((position) => {
     doSomething(position.coords.latitude, position.coords.longitude);
   });
 
-function doSomething(x, y) {
-    console.log(x, y);
+function doSomething(lat, long) {
+    // send API to get weather data
+    const xhttpr = new XMLHttpRequest();
+    xhttpr.open('GET', 'https://api.openweathermap.org/data/2.5/weather?lat='+ lat + '&lon=' + long + '&appid=8426cd40d49e2f654dd794fb602201e0', true);
+    
+    xhttpr.send();
+    
+    xhttpr.onload = ()=> {
+    if (xhttpr.status === 200) {
+        const response = JSON.parse(xhttpr.response);
+        console.log(response);
+    } else {
+        // Handle error
+    }
+    };
 }
