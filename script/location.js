3b892ebd54db8bc6dc3c6d321c600c3333f51c7b
Andy <66284061+flipperflopper@users.noreply.github.com>
2024-01-19 12:18:33 +0800

    test

--- script/location.js
+++ script/location.js
@@ -1,6 +1,7 @@
-if ("geolocation" in navigator) {
-    console.log("Yes");
-  } else {
-    console.log("No");
-  }
-  
+navigator.geolocation.getCurrentPosition((position) => {
+    doSomething(position.coords.latitude, position.coords.longitude);
+  });
+
+function doSomething(x, y) {
+    console.log(x, y);
+}
