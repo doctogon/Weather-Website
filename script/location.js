14df0d897f004d7b2e8ce7b306ffbaf8d1971b0f
Andy <66284061+flipperflopper@users.noreply.github.com>
2024-01-19 12:10:08 +0800

    test

--- script/location.js
+++ script/location.js
@@ -1,9 +1,6 @@
-// // url: https://api.ipgeolocation.io/ipgeo?apiKey=e88203af81524391a7641693aa2d7430&ip=<ADD IP HERE>
-
-// // find ip of the user
-// function getIPFromAmazon() {
-//     fetch("https://checkip.amazonaws.com/").then(res => res.text()).then(data => console.log(data))
-//   }
-
+if ("geolocation" in navigator) {
+    console.log("Yes");
+  } else {
+    console.log("No");
+  }
   
-// document.getElementById("#gfg").innerHTML = getIPFromAmazon();
